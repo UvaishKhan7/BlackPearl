2fdff58a6d273fd122024595a9de7fc558819688
Uvaish Khan <106305113+UvaishKhan7@users.noreply.github.com>
2022-07-04 17:53:44 +0530

    Create Generate_Random_Whole_Numbers_within_a_Range.js

---
+++ Generate_Random_Whole_Numbers_within_a_Range.js
@@ -0,0 +1,4 @@
+//Create a function called randomRange that takes a range myMin and myMax and returns a random whole number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+}
