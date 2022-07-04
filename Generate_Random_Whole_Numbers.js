ea3ecc7880914b1813df351f4f3ee1056b171dcf
Uvaish Khan <106305113+UvaishKhan7@users.noreply.github.com>
2022-07-04 17:50:00 +0530

    Create Generate_Random_Whole_Numbers.js

---
+++ Generate_Random_Whole_Numbers.js
@@ -0,0 +1,8 @@
+//Use this technique to generate and return a random whole number between 0 and 9.
+
+function randomWholeNum() {
+  return Math.floor(Math.random()*10);
+}
+
+//Use Math.random() to generate a random decimal.
+//Use ath.floor() to round the number down to its nearest whole number.
